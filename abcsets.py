db43d015836866364d0e1e8e9aec0abe2a517b31
Stephan Deibel <sdeibel@wingware.com>
2015-01-24 09:31:05 -0500

    Removed junk file

--- abcsets.py
+++
@@ -1,7 +0,0 @@
-# Generate Tune Jam cheat sheets for dance sets
-# Written by Stephan Deibel
-# January 2015
-
-cmd = "bin/abcm2ps jennylind.abc -O jennylind.ps"
-
-usage = "python abcsets.py outfile.pdf tune1 tune2 tune3 ..."
