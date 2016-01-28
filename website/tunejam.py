b587c4f3b43fe87bd34b59d044f163163de35e36
Stephan Deibel <sdeibel@wingware.com>
2016-01-27 22:13:14 -0500

    Attempt to fix sets page for devices

--- website/tunejam.py
+++ website/tunejam.py
@@ -137,6 +137,7 @@ def sets(spec=None):
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script src="/js/jquery.sortElements.js"></script>
+<script src="https://raw.github.com/furf/jquery-ui-touch-punch/master/jquery.ui.touch-punch.min.js"></script> 
  <script>
 $(function() {
   $( "#alltunes, #selectedtunes" ).sortable({
